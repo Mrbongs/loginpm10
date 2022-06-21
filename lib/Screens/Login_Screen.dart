e433214122c0307cb33eb2c350978cc34f594fa8
AdiNugroho <wedding21agustus@gmail.com>
2022-06-21 19:53:36 +0700

    second commit

---
+++ lib/Screens/Login_Screen.dart
@@ -0,0 +1,176 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todolistapp/Component/button.dart';
+import '../constants.dart';
+import 'Home_Screen.dart';
+import 'Register_Screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final formkey = GlobalKey<FormState>();
+  final _auth = FirebaseAuth.instance;
+  String email = '';
+  String password = '';
+  bool isloading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: isloading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Form(
+              key: formkey,
+              child: AnnotatedRegion<SystemUiOverlayStyle>(
+                value: SystemUiOverlayStyle.light,
+                child: Stack(
+                  children: [
+                    Container(
+                      height: double.infinity,
+                      width: double.infinity,
+                      color: Color(0xF7F6F2),
+                      child: SingleChildScrollView(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 25, vertical: 120),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Login",
+                              style: TextStyle(
+                                  fontSize: 50,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(height: 30),
+                            TextFormField(
+                              keyboardType: TextInputType.emailAddress,
+                              onChanged: (value) {
+                                email = value;
+                              },
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return "Masukan Email";
+                                }
+                              },
+                              textAlign: TextAlign.center,
+                              decoration: kTextFieldDecoration.copyWith(
+                                hintText: 'Email',
+                                prefixIcon: Icon(
+                                  Icons.email,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 30),
+                            TextFormField(
+                              obscureText: true,
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return "Masukan Password";
+                                }
+                              },
+                              onChanged: (value) {
+                                password = value;
+                              },
+                              textAlign: TextAlign.center,
+                              decoration: kTextFieldDecoration.copyWith(
+                                  hintText: 'Password',
+                                  prefixIcon: Icon(
+                                    Icons.lock,
+                                    color: Colors.black,
+                                  )),
+                            ),
+                            SizedBox(height: 80),
+                            LoginSignupButton(
+                              title: 'Login',
+                              ontapp: () async {
+                                if (formkey.currentState!.validate()) {
+                                  setState(() {
+                                    isloading = true;
+                                  });
+                                  try {
+                                    await _auth.signInWithEmailAndPassword(
+                                        email: email, password: password);
+
+                                    await Navigator.of(context).push(
+                                      MaterialPageRoute(
+                                        builder: (contex) => HomeScreen(),
+                                      ),
+                                    );
+
+                                    setState(() {
+                                      isloading = false;
+                                    });
+                                  } on FirebaseAuthException catch (e) {
+                                    showDialog(
+                                      context: context,
+                                      builder: (ctx) => AlertDialog(
+                                        title: Text("Ops! Login Gagal!"),
+                                        content: Text('${e.message}'),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(ctx).pop();
+                                            },
+                                            child: Text('Okay'),
+                                          )
+                                        ],
+                                      ),
+                                    );
+                                    print(e);
+                                  }
+                                  setState(() {
+                                    isloading = false;
+                                  });
+                                }
+                              },
+                            ),
+                            SizedBox(height: 30),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) => SignupScreen(),
+                                  ),
+                                );
+                              },
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "Tidak Punya Akun?",
+                                    style: TextStyle(
+                                        fontSize: 20, color: Colors.black87),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Hero(
+                                    tag: '1',
+                                    child: Text(
+                                      'Register',
+                                      style: TextStyle(
+                                          fontSize: 21,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+    );
+  }
+}
